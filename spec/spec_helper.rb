3cb0a516d814ecc2ee7b6e52721f277b66db606f
iyke <nderiony95@gmail.com>
2017-07-21 17:16:28 +0300

    finished spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,19 @@ require 'bundler/setup'
 Bundler.reqire(:default, :test)
 set(:root, Dir.pwd())
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+require 'capybara/rspec'
+require './app'
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+RSpec.configure { |config| 
+	config.after(:each) {
+		Store.all.each { |store|
+			store.destroy
+		}
+		Brand.all.each {|brand|
+			brand.destroy
+		}
+	}
+}
