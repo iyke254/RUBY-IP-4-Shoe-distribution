f9ad50107616f8b84a0efb6ef15d488836c06abd
iyke <nderiony95@gmail.com>
2017-07-21 12:52:30 +0300

    ran migration, the tables are now created in the db plus the schema file is created

--- db/migrate/20170721092801_brands_stores.rb
+++ db/migrate/20170721092801_brands_stores.rb
@@ -1,4 +1,8 @@
 class BrandsStores < ActiveRecord::Migration[5.1]
   def change
+  	create_table(:brands_stores) do |t|
+  		t.column(:brand_id, :integer)
+  		t.column(:store_id, :integer)
+  	end
   end
 end
